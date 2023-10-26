6675a8628f4be9828d6ec08bea78fb9bb86fa530
rheins22 <127905208+rheins22@users.noreply.github.com>
2023-10-25 20:41:56 -0500

    building out controllers

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("eventCategories")
+public class EventCategoryController {
+
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping()
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/index";
+    }
+
+    public String renderCreateEventCategoryForm() {
+
+        return "";
+    }
+
+    public String CreateEventCategoryForm() {
+
+        return "";
+    }
+
+}
